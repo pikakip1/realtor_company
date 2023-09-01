58559ebd585b09cad5a7c3b3ecfc88cf12eea605
pikakip1 <pikakip222@mail.ru>
2023-09-01 15:51:31 +0300

    add env file

--- real_estate_agency/settings.py
+++ real_estate_agency/settings.py
@@ -4,7 +4,7 @@ import os
 from environs import Env
 
 env = Env()
-env.read_env()
+env.read_env('sensitive information.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
