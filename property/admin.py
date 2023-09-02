eafe11b70730ff2d9cd428e54e2118b91cd0625d
pikakip1 <pikakip222@mail.ru>
2023-09-02 17:45:35 +0300

    added the output of links in the admin panel

--- property/admin.py
+++ property/admin.py
@@ -3,18 +3,27 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class PropertyOwnersInline(admin.TabularInline):
+    model = Owner.owner_flat.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['address', 'town']
     readonly_fields = ['created_at']
     list_display = ['town', 'address', 'price', 'owners_phonenumber', 'new_building', 'construction_year']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
-
     raw_id_fields = ('likes',)
+
+    inlines = [PropertyOwnersInline]
 
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('owner_flat',)
+
+    inlines = [PropertyOwnersInline]
+    exclude = ['owner_flat']
 
 
 class ComplaintAdmin(admin.ModelAdmin):
@@ -24,3 +33,4 @@ class ComplaintAdmin(admin.ModelAdmin):
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
 admin.site.register(Owner, OwnerAdmin)
+
